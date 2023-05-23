403826992a30a85b00a67192ce69578bc4afc153
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-22 22:21:28 -0500

    Creating event onClick to that a function named cambiarNombre start it

--- src/components/MiPrimerEstado.js
+++ src/components/MiPrimerEstado.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const MiPrimerEstado = () => {
-  return (
-    <div>MiPrimerEstado</div>
+  
+    let nombre = "Alex Villegas";
+
+    const cambiarNombre = e => {
+        nombre = "Evita Velarde";
+    };
+  
+    return (
+    <div>
+        
+        <h3>COMPONENTE: MiPrimerEstado</h3>
+        <strong>
+            {nombre}
+        </strong>
+
+        <button onClick={ cambiarNombre }Cambiar></button>
+    </div>
   )
 }
