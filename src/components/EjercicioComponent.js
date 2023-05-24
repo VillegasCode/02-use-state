2751592a3a5870b360c4ccfe8ed5b1c15b97abaf
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-24 14:18:58 -0500

    The prop {year} of EjercicioComponent becomes a function that return the year into of strong label with className label label-green

--- src/components/EjercicioComponent.js
+++ src/components/EjercicioComponent.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export const EjercicioComponent = () => {
+export const EjercicioComponent = ({year}) => {
   return (
     <div>
         <h2>Ejercicio con Eventos y useState</h2>
-
+        <strong className='label label-green'>
+            {year}
+        </strong>
     </div>
   )
 }
