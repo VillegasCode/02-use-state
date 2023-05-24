0beb020f17783a0e4c63fd44b06a57a18c706573
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-24 17:49:14 -0500

    We create 3 functions: siguiente, anterior, reset and add us 3 buttons

--- src/components/EjercicioComponent.js
+++ src/components/EjercicioComponent.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const EjercicioComponent = ({year}) => {
+  
+  // Convirtiendo nuestra variable en reactiva
+  const [yearNow, setYearNow] = useState(year);
+
+  // Aumentar el año de 1 en 1
+  const siguiente = e => {
+    setYearNow(yearNow + 1);
+  }
+
+  // Reducir el año de 1 en 1
+  const anterior = e => {
+    setYearNow(yearNow - 1);
+  }
+
+  //Restablecer a año actual
+  const fecha = new Date();
+  let yearActual = e => {
+    yearActual = fecha.getFullYear();
+    setYearNow(yearActual); 
+  }
+  
   return (
     <div>
         <h2>Ejercicio con Eventos y useState</h2>
         <strong className='label label-green'>
-            {year}
+            {yearNow}
         </strong>
+        <p>
+          <button onClick={anterior}>Anterior</button>
+          &nbsp;
+        <button onClick={siguiente}>Siguiente</button>
+          &nbsp;
+        <button onClick={yearActual}>Reset</button>
+        </p>
     </div>
   )
 }
